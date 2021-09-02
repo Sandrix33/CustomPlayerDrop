4af5d749121708d8d01a04b001f1eba0d39e6373
Sandrix33 <79366455+Sandrix33@users.noreply.github.com>
2021-09-02 13:29:16 +0200

    Add files via upload

---
+++ src/CustomPlayerDrop/Sandrix/Dev/Main.java
@@ -0,0 +1,20 @@
+package CustomPlayerDrop.Sandrix.Dev;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import CustomPlayerDrop.Sandrix.Dev.Config.Config;
+import CustomPlayerDrop.Sandrix.Dev.Config.Reload;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+	
+		Config.CheckConfig();
+		Config.loadConfig();
+		Bukkit.getServer().getPluginManager().registerEvents(new Event(), this);
+		getCommand("CustomPlayerDrop").setExecutor(new Reload());
+		
+		
+	}
+}
